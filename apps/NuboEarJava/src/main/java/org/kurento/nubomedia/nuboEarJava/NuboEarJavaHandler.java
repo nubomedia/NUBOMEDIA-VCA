9a5b9f2e666aaad453d5bb6bd1ac6967e88686ba
Victor Hidalgo <vmhidalgo@visual-tools.com>
2015-10-02 12:50:14 +0200

    Ear detector added

---
+++ apps/NuboEarJava/src/main/java/org/kurento/nubomedia/nuboEarJava/NuboEarJavaHandler.java
@@ -0,0 +1,248 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package org.kurento.nubomedia.nuboEarJava;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.kurento.client.EventListener;
+import org.kurento.client.IceCandidate;
+import org.kurento.client.KurentoClient;
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.OnIceCandidateEvent;
+import org.kurento.client.WebRtcEndpoint;
+import org.kurento.jsonrpc.JsonUtils;
+import org.kurento.module.nuboeardetector.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+/**
+ * Chroma handler (application and media logic).
+ * 
+ * @author Boni Garcia (bgarcia@gsyc.es)
+ * @author David Fernandez (d.fernandezlop@gmail.com)
+ * @since 6.0.0
+ */
+public class NuboEarJavaHandler extends TextWebSocketHandler {
+
+	private final Logger log = LoggerFactory.getLogger(NuboEarJavaHandler.class);
+	private static final Gson gson = new GsonBuilder().create();
+
+	private final ConcurrentHashMap<String, UserSession> users = new ConcurrentHashMap<String, UserSession>();
+
+	@Autowired
+	private KurentoClient kurento;
+
+	private NuboEarDetector ear = null;
+	private int visualizeEar = -1;
+
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message)
+			throws Exception {
+		JsonObject jsonMessage = gson.fromJson(message.getPayload(),
+				JsonObject.class);
+
+		log.debug("Incoming message: {}", jsonMessage);
+
+		switch (jsonMessage.get("id").getAsString()) {
+		case "start":
+			start(session, jsonMessage);
+			break;
+		case "show_ears":	
+			setVisualization(session,jsonMessage);
+			break;
+		case "scale_factor":
+			setScaleFactor(session,jsonMessage);
+		    break;
+		case "process_num_frames":
+			setProcessNumberFrames(session,jsonMessage);
+		    break;
+		case "width_to_process":
+			setWidthToProcess(session,jsonMessage);
+		    break;			
+		case "stop": {
+			UserSession user = users.remove(session.getId());
+			if (user != null) {
+				user.release();
+			}
+			break;
+		}
+		case "onIceCandidate": {
+			JsonObject candidate = jsonMessage.get("candidate")
+					.getAsJsonObject();
+
+			UserSession user = users.get(session.getId());
+			if (user != null) {
+				IceCandidate cand = new IceCandidate(candidate.get("candidate")
+						.getAsString(), candidate.get("sdpMid").getAsString(),
+						candidate.get("sdpMLineIndex").getAsInt());
+				user.addCandidate(cand);
+			}
+			break;
+		}
+
+		default:
+			sendError(session,
+					"Invalid message with id "
+							+ jsonMessage.get("id").getAsString());
+			break;
+		}
+	}
+
+	private void start(final WebSocketSession session, JsonObject jsonMessage) {
+		try {
+			// Media Logic (Media Pipeline and Elements)
+			UserSession user = new UserSession();
+			MediaPipeline pipeline = kurento.createMediaPipeline();
+			user.setMediaPipeline(pipeline);
+			WebRtcEndpoint webRtcEndpoint = new WebRtcEndpoint.Builder(pipeline)
+					.build();
+			user.setWebRtcEndpoint(webRtcEndpoint);
+			users.put(session.getId(), user);
+
+			webRtcEndpoint
+					.addOnIceCandidateListener(new EventListener<OnIceCandidateEvent>() {
+
+						@Override
+						public void onEvent(OnIceCandidateEvent event) {
+							JsonObject response = new JsonObject();
+							response.addProperty("id", "iceCandidate");
+							response.add("candidate", JsonUtils
+									.toJsonObject(event.getCandidate()));
+							try {
+								synchronized (session) {
+									session.sendMessage(new TextMessage(
+											response.toString()));
+								}
+							} catch (IOException e) {
+								log.debug(e.getMessage());
+							}
+						}
+					});
+
+			ear = new NuboEarDetector.Builder(pipeline).build();
+
+			
+			webRtcEndpoint.connect(ear);
+			ear.connect(webRtcEndpoint);
+
+			// SDP negotiation (offer and answer)
+			String sdpOffer = jsonMessage.get("sdpOffer").getAsString();
+			String sdpAnswer = webRtcEndpoint.processOffer(sdpOffer);
+
+			// Sending response back to client
+			JsonObject response = new JsonObject();
+			response.addProperty("id", "startResponse");
+			response.addProperty("sdpAnswer", sdpAnswer);
+
+			synchronized (session) {
+				session.sendMessage(new TextMessage(response.toString()));
+			}
+			webRtcEndpoint.gatherCandidates();
+
+		} catch (Throwable t) {
+			sendError(session, t.getMessage());
+		}
+	}
+
+	private void setVisualization(WebSocketSession session,JsonObject jsonObject)
+        {
+
+               	try{
+                       	visualizeEar = jsonObject.get("val").getAsInt();
+                        if (null != ear)
+                        {
+                        		System.out.println("Visuzlize Ear .... " + visualizeEar);
+       	                        ear.showEars(visualizeEar);
+                        }
+
+               	} catch (Throwable t){
+                       	sendError(session,t.getMessage());
+                }
+        }
+	
+	private void setScaleFactor(WebSocketSession session,JsonObject jsonObject)
+    {
+	
+	try{
+	    int scale = jsonObject.get("val").getAsInt();
+	    
+	    if (null != ear)
+		{
+		    log.debug("Sending setscaleFactor...." + scale);		  
+		    ear.multiScaleFactor(scale);
+		}
+	    
+	} catch (Throwable t){
+	    sendError(session,t.getMessage());
+	}
+    }
+
+    private void setProcessNumberFrames(WebSocketSession session,JsonObject jsonObject)
+    {
+	
+	try{
+	    int num_img = jsonObject.get("val").getAsInt();
+	    
+	    if (null != ear)
+		{
+		    log.debug("Sending process num frames...." + num_img);
+		    
+		    ear.processXevery4Frames(num_img);
+ 		}
+	    
+	} catch (Throwable t){
+	    sendError(session,t.getMessage());
+	}
+    }
+		
+    private void setWidthToProcess(WebSocketSession session,JsonObject jsonObject)
+    {
+	
+	try{
+	    int width = jsonObject.get("val").getAsInt();
+	    
+	    if (null != ear)
+		{
+		    log.debug("Sending width...." + width);
+		    ear.widthToProcess(width);
+		}
+	    
+	} catch (Throwable t){
+	    sendError(session,t.getMessage());
+	}
+    } 
+
+
+
+	private void sendError(WebSocketSession session, String message) {
+		try {
+			JsonObject response = new JsonObject();
+			response.addProperty("id", "error");
+			response.addProperty("message", message);
+			session.sendMessage(new TextMessage(response.toString()));
+		} catch (IOException e) {
+			log.error("Exception sending message", e);
+		}
+	}
+}
