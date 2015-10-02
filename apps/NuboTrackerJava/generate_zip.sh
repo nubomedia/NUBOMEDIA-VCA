1d493f7e5dd2ccab21af0374110c14a58781d240
Victor Hidalgo <vmhidalgo@visual-tools.com>
2015-10-02 12:54:29 +0200

    Tracker demo added

---
+++ apps/NuboTrackerJava/generate_zip.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+APP_NAME=NuboTrackerJava
+VERSION=6.0.0
+DIR_INSTALL=apps_install
+
+mkdir -p ../$DIR_INSTALL
+rm -f install/*zip install/*jar
+mvn package
+cp target/$APP_NAME-$VERSION.jar install/
+cd install; mv $APP_NAME-$VERSION.jar $APP_NAME.jar; zip $APP_NAME.zip *sh *jar;
+cp $APP_NAME.zip ../../$DIR_INSTALL
+
+echo "Done!"
