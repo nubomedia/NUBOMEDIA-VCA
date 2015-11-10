59fce5d9e11dbbc142b0941366214b25e42e3d69
Victor Hidalgo <vmhidalgo@visual-tools.com>
2015-11-10 17:10:04 +0100

    Minor error fixed in build scripts

--- generate_deb.sh
+++ generate_deb.sh
@@ -5,24 +5,24 @@ DEB_PACK=$OUTPUT/packages
 JS_API=$OUTPUT/js_api
 
 CURRENT_PATH=../../../
-#cd modules/nubo_ear/nubo-ear-detector/ ;     sh compile_filter.sh;  sh compile_filter.sh js; cd $CURRENT_PATH
-#cd modules/nubo_eye/nubo-eye-detector/ ;     sh compile_filter.sh; sh compile_filter.sh js; cd $CURRENT_PATH
-#cd modules/nubo_face/nubo-face-detector/ ;   sh compile_filter.sh; sh compile_filter.sh js; cd $CURRENT_PATH
-#cd modules/nubo_mouth/nubo-mouth-detector/ ; sh compile_filter.sh; sh compile_filter.sh js; cd $CURRENT_PATH
-#cd modules/nubo_nose/nubo-nose-detector/ ;   sh compile_filter.sh; sh compile_filter.sh js; cd $CURRENT_PATH
-#cd modules/nubo_tracker/nubo-tracker/ ;      sh compile_filter.sh; sh compile_filter.sh js; cd $CURRENT_PATH
+cd modules/nubo_ear/nubo-ear-detector/ ;     sh compile_filter.sh;  sh compile_filter.sh js; cd $CURRENT_PATH
+cd modules/nubo_eye/nubo-eye-detector/ ;     sh compile_filter.sh; sh compile_filter.sh js; cd $CURRENT_PATH
+cd modules/nubo_face/nubo-face-detector/ ;   sh compile_filter.sh; sh compile_filter.sh js; cd $CURRENT_PATH
+cd modules/nubo_mouth/nubo-mouth-detector/ ; sh compile_filter.sh; sh compile_filter.sh js; cd $CURRENT_PATH
+cd modules/nubo_nose/nubo-nose-detector/ ;   sh compile_filter.sh; sh compile_filter.sh js; cd $CURRENT_PATH
+cd modules/nubo_tracker/nubo-tracker/ ;      sh compile_filter.sh; sh compile_filter.sh js; cd $CURRENT_PATH
 
-#mkdir -p $DEB_PACK
-#mkdir -p $JS_API
-#rm -f $DEB_PACK/*
-#rm -rf $JS_API/*
+mkdir -p $DEB_PACK
+mkdir -p $JS_API
+rm -f $DEB_PACK/*
+rm -rf $JS_API/*
 
-#cp modules/nubo_ear/*deb $DEB_PACK
-#cp modules/nubo_eye/*deb $DEB_PACK
-#cp modules/nubo_face/*deb $DEB_PACK
-#cp modules/nubo_mouth/*deb $DEB_PACK
-#cp modules/nubo_nose/*deb $DEB_PACK
-#cp modules/nubo_tracker/*deb $DEB_PACK
+cp modules/nubo_ear/*deb $DEB_PACK
+cp modules/nubo_eye/*deb $DEB_PACK
+cp modules/nubo_face/*deb $DEB_PACK
+cp modules/nubo_mouth/*deb $DEB_PACK
+cp modules/nubo_nose/*deb $DEB_PACK
+cp modules/nubo_tracker/*deb $DEB_PACK
 
 
 mkdir -p $JS_API/nuboeardetector/js/   $JS_API/nuboeyedetector/js/  $JS_API/nubofacedetector/js/
