6189a192a14b3bb86ab12ae1ae3373cee3f709bf
Boni Garcia <bgarcia@gsyc.es>
2016-11-02 17:26:50 +0100

    Scripts cleanup

--- generate_apps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-OUTPUT=./output
-APPS=apps
-OUT_APPS=$OUTPUT/apps
-
-mkdir -p $OUT_APPS
-
-cd $APPS/; sh generate_apps.sh; cp -r  apps_install/*zip  ../$OUT_APPS
--- modules/nubo_ear/nubo-ear-detector/compile_filter.sh
+++ modules/nubo_ear/nubo-ear-detector/compile_filter.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
 mkdir -p build
-if [ "$1" = "js" ]; then
-    cd build;  cmake .. -DGENERATE_JS_CLIENT_PROJECT=TRUE;  cd js; sudo npm install grunt grunt-browserify grunt-contrib-clean grunt-jsdoc grunt-npm2bower-sync minifyify; grunt    
-else 
-    cd build; cmake ../; sleep 2 ; make; sleep 2 ; sudo make install;  cmake ../  -DGENERATE_JAVA_CLIENT_PROJECT=TRUE; sleep 2; 
-    sed 's/org.kurento.module/com.visual-tools.nubomedia/g' ./java/pom.xml > ./java/pom_ok.xml; mv ./java/pom_ok.xml ./java/pom.xml; make java_install
-    sudo debuild -us -uc
-fi
-
+cd build; cmake ../; make; cmake ../ -DGENERATE_JAVA_CLIENT_PROJECT=TRUE;
+debuild -us -uc
